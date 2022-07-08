6a62e527e6963aaa18b430f29756ee9c0514547a
jinyoung <danroom85@gmail.com>
2022-07-08 19:35:45 +0900

    firebase auth add

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import {authService} from "fbase";
+function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
